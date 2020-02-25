f8645088414a66a0002a8911c99d44225a4f837f
ArSquareDev <oneforpatient@gmail.com>
2020-02-25 16:32:43 +0530

    Second commit

---
+++ One for Patient/Global CLass/AuthenticationVIewController .swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticationViewController.swift
+//  One for Patient
+//
+//  Created by Idontknow on 14/09/19.
+//  Copyright © 2019 AnnantaSourceLLc. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    
+
+}
